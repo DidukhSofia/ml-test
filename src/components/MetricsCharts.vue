c89fb09bf3d207f0689840a3a989954aa3e39416
User <diduhs0@gmail.com>
2025-08-06 17:09:47 +0300

    Add styling

--- src/components/MetricsCharts.vue
+++ src/components/MetricsCharts.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="charts-scroll-wrapper">
-    <div class="charts-container">
-      <div
-        v-for="(metric, metricName) in metricsByName"
-        :key="metricName"
-        class="chart-wrapper"
-      >
-        <h3>{{ metricName }}</h3>
-        <Line :data="generateChartData(metricName)" :options="chartOptions" />
-      </div>
+  <div class="charts-container">
+    <div
+      v-for="metricName in metricNames"
+      :key="metricName"
+      class="chart-wrapper"
+    >
+      <h3>{{ metricName }}</h3>
+      <Line
+        :data="generateChartData(metricName)"
+        :options="chartOptions"
+        :height="400"
+        :width="null" 
+      />
     </div>
   </div>
 </template>
-
 
 <script>
 import { Line } from "vue-chartjs";
@@ -24,7 +26,7 @@ import {
   LineElement,
   PointElement,
   CategoryScale,
-  LinearScale,
+  LinearScale
 } from "chart.js";
 
 ChartJS.register(
@@ -38,121 +40,66 @@ ChartJS.register(
 );
 
 export default {
-  name: "MetricCharts",
-  components: {
-    Line,
-  },
+  name: "MetricsCharts",
+  components: { Line },
   props: {
-    processedData: {
-      type: Object,
-      required: true,
-    },
+    processedData: Object,
+    experimentColors: Object
   },
   computed: {
-    metricsByName() {
-      const metrics = {};
-      for (const experimentId in this.processedData) {
-        const experimentMetrics = this.processedData[experimentId];
-        for (const metricName in experimentMetrics) {
-          if (!metrics[metricName]) {
-            metrics[metricName] = {};
-          }
-          metrics[metricName][experimentId] = experimentMetrics[metricName];
-        }
-      }
-      return metrics;
+    metricNames() {
+      const firstId = Object.keys(this.processedData)[0];
+      return Object.keys(this.processedData[firstId]);
     },
+    chartOptions() {
+      return {
+        responsive: true,
+        maintainAspectRatio: false, // щоб висота працювала
+        plugins: {
+          legend: { position: "top" },
+          tooltip: { mode: "index", intersect: false }
+        },
+        interaction: { mode: "nearest", axis: "x", intersect: false },
+        elements: {
+          point: {
+            radius: 0 // прибирає кружечки
+          }
+        }
+      };
+    }
   },
   methods: {
     generateChartData(metricName) {
-      const metricData = this.metricsByName[metricName];
       return {
-        labels: this.generateLabels(metricData),
-        datasets: Object.entries(metricData).map(
-          ([experimentId, points], index) => ({
-            label: experimentId,
-            data: points.map((p) => p.value),
-            borderColor: this.getColor(index),
-            fill: false,
-            tension: 0.2,
-          })
-        ),
+        labels: this.processedData[Object.keys(this.processedData)[0]][
+          metricName
+        ].map((p) => p.step),
+        datasets: Object.keys(this.processedData).map((id) => ({
+          label: id,
+          data: this.processedData[id][metricName].map((p) => p.value),
+          borderColor: this.experimentColors[id],
+          backgroundColor: this.experimentColors[id],
+          fill: false
+        }))
       };
-    },
-    generateLabels(metricData) {
-      const allSteps = new Set();
-      for (const points of Object.values(metricData)) {
-        points.forEach((p) => allSteps.add(p.step));
-      }
-      return Array.from(allSteps).sort((a, b) => a - b);
-    },
-    getColor(index) {
-      const colors = [
-        "#FF6384",
-        "#36A2EB",
-        "#FFCE56",
-        "#4BC0C0",
-        "#9966FF",
-        "#FF9F40",
-        "#E7E9ED",
-        "#8B0000",
-      ];
-      return colors[index % colors.length];
-    },
-  },
-  data() {
-    return {
-      chartOptions: {
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins: {
-          legend: {
-            position: "bottom",
-          },
-        },
-        scales: {
-          x: {
-            title: {
-              display: true,
-              text: "Step",
-            },
-          },
-          y: {
-            title: {
-              display: true,
-              text: "Value",
-            },
-          },
-        },
-        elements: {
-        point: {
-          radius: 0,
-          hoverRadius: 0,
-        },
-      },
-      },
-    };
-  },
+    }
+  }
 };
 </script>
 
 <style scoped>
-.charts-scroll-wrapper {
-  overflow-x: auto;
-  padding-bottom: 1rem;
-}
-
 .charts-container {
-  display: flex;
-  flex-direction: column;
-  gap: 3rem;
-  margin-top: 2rem;
-  min-width: 900px;
+  width: 100%;
+  /* overflow: hidden; */
 }
-
 .chart-wrapper {
+  padding: 20px;
+  width: 100%;
+  min-width: 900px;
+  /* overflow: hidden; */
   height: 400px;
-  width: 2000px; /* Фіксована ширина */
 }
-
-</style>
+canvas {
+  overflow-x: scroll;
+}
+</style> 
